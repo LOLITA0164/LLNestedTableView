29b68cbea00cd9a3e203fbad20c96f5166df206d
LOLITA <476512340@qq.com>
2017-09-19 16:52:27 +0800

    Initial Commit

---
+++ NestedTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NestedTableView
+//
+//  Created by LOLITA on 2017/9/19.
+//  Copyright © 2017年 LOLITA0164. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
